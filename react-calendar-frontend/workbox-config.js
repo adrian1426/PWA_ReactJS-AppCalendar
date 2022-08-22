b7a1ee75b6d4c5656a8f59672ded3e28f785b6a0
ADRIAN HERNANDEZ <adrianhh1426@gmail.com>
2022-08-21 21:06:02 -0500

    Agregando workbox y cmd para build

--- react-calendar-frontend/workbox-config.js
+++ react-calendar-frontend/workbox-config.js
@@ -4,8 +4,9 @@ module.exports = {
 		'**/*.{json,ico,html,png,txt,css,js}'
 	],
 	swDest: 'build/sw.js',
-	ignoreURLParametersMatching: [
-		/^utm_/,
-		/^fbclid$/
-	]
+	// ignoreURLParametersMatching: [
+	// 	/^utm_/,
+	// 	/^fbclid$/
+	// ],
+	swSrc: 'src/sw-template.js'
 };
