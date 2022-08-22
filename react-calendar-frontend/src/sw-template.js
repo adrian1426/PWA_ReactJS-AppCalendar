dfdb76cc1e1d4a3215c7f6950a74e4707b51501b
ADRIAN HERNANDEZ <adrianhh1426@gmail.com>
2022-08-21 21:38:35 -0500

    get por NetwotkFirst

--- react-calendar-frontend/src/sw-template.js
+++ react-calendar-frontend/src/sw-template.js
@@ -1,3 +1,26 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js');
 
 workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
+
+const { registerRoute } = workbox.routing;
+const { CacheFirst, NetworkFirst } = workbox.strategies;
+
+registerRoute(
+  new RegExp('http://localhost:4000/api/auth/renew'),
+  new NetworkFirst()
+);
+
+registerRoute(
+  new RegExp('http://localhost:4000/api/events'),
+  new NetworkFirst()
+);
+
+registerRoute(
+  new RegExp('https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css'),
+  new CacheFirst()
+);
+
+registerRoute(
+  new RegExp('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/all.min.css'),
+  new CacheFirst()
+);
