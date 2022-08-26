d35b74d930ff828fb42cc296acff5fdc6fb671b7
ADRIAN HERNANDEZ <adrianhh1426@gmail.com>
2022-08-25 22:50:53 -0500

    post offline rest

--- react-calendar-frontend/src/sw-template.js
+++ react-calendar-frontend/src/sw-template.js
@@ -1,9 +1,12 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js');
 
+workbox.loadModule('workbox-background-sync');
+
 workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
 
 const { registerRoute } = workbox.routing;
-const { CacheFirst, NetworkFirst } = workbox.strategies;
+const { CacheFirst, NetworkFirst, NetworkOnly } = workbox.strategies;
+const { BackgroundSyncPlugin } = workbox.backgroundSync;
 
 registerRoute(
   new RegExp('http://localhost:4000/api/auth/renew'),
@@ -23,4 +26,17 @@ registerRoute(
 registerRoute(
   new RegExp('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/all.min.css'),
   new CacheFirst()
+);
+
+//Post Offline
+const bgSyncPlugin = new BackgroundSyncPlugin('post-offline', {
+  maxRetentionTime: 24 * 60 //24 hr
+});
+
+registerRoute(
+  new RegExp('http://localhost:4000/api/events'),
+  new NetworkOnly({
+    plugins: [bgSyncPlugin]
+  }),
+  'POST'
 );
