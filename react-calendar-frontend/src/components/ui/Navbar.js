508f037b53be8770dbe227da6020518b5bf936f3
ADRIAN HERNANDEZ <adrianhh1426@gmail.com>
2022-08-26 21:30:52 -0500

    componente offline y online detector

--- react-calendar-frontend/src/components/ui/Navbar.js
+++ react-calendar-frontend/src/components/ui/Navbar.js
@@ -1,25 +1,29 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { startLogout } from '../../actions/auth';
+import { Offline, Online } from 'react-detect-offline';
 
 export const Navbar = () => {
 
     const dispatch = useDispatch();
-    const { name } = useSelector( state => state.auth );
+    const { name } = useSelector(state => state.auth);
 
     const handleLogout = () => {
-        dispatch( startLogout() );
+        dispatch(startLogout());
     }
 
     return (
         <div className="navbar navbar-dark bg-dark mb-4">
             <span className="navbar-brand">
-                { name }
+                {name}
             </span>
-            
-            <button 
+
+            <Online><span className="text-success">Están online</span></Online>
+            <Offline><span className="text-danger">No tienes conexión a internet</span></Offline>
+
+            <button
                 className="btn btn-outline-danger"
-                onClick={ handleLogout }
+                onClick={handleLogout}
             >
                 <i className="fas fa-sign-out-alt"></i>
                 <span> Salir</span>
